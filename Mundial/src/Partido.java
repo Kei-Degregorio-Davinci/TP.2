81603b2ec9d55c4c422acf836b51d719d07a6918
Keitty <Keila.degregorio@davinci.edu.ar>
2022-12-05 01:28:56 -0300

    feat: add setter and getter in class Partido

--- Mundial/src/Partido.java
+++ Mundial/src/Partido.java
@@ -1,2 +1,69 @@
+import java.util.Date;
+
 public class Partido {
+    private Date fecha;
+    private Equipo local;
+    private Equipo visitante;
+    private Resultado resultado;
+
+    public Partido(Date fecha, Equipo local, Equipo visitante) {
+        this.fecha = fecha;
+        this.local = local;
+        this.visitante = visitante;
+
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Equipo getLocal() {
+        return local;
+    }
+
+    public void setLocal(Equipo local) {
+        this.local = local;
+    }
+
+    public Equipo getVisitante() {
+        return visitante;
+    }
+
+    public void setVisitante(Equipo visitante) {
+        this.visitante = visitante;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Resultado resultado) {
+        this.resultado = resultado;
+        this.local.addPartidosJugados(this);
+        this.visitante.addPartidosJugados(this);
+        if (this.getResultado().ganoLocal()){
+            this.local.partidoGanado();
+        }
+        else if (this.getResultado().empate()){
+            this.local.partidoEmpatado();
+            this.visitante.partidoEmpatado();
+        } else {
+            this.visitante.partidoGanado();
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Partido{" +
+                "fecha=" + fecha +
+                ", local=" + local +
+                ", visitante=" + visitante +
+                ", resultado=" + resultado +
+                '}';
+    }
 }
