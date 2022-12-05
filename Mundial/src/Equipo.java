01e551e5cd41580ae6cd7bbd631f1ee2c4e0fef9
Keitty <Keila.degregorio@davinci.edu.ar>
2022-12-05 01:24:47 -0300

    feat: add setter and getter in Class Equipo

--- Mundial/src/Equipo.java
+++ Mundial/src/Equipo.java
@@ -1,3 +1,63 @@
-public class Equipo {
+import java.util.ArrayList;
 
+public class Equipo {
+    private String nombre;
+    private ArrayList<Partido> partidosJugados;
+    private int puntajeEtapaGrupos;
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+        this.partidosJugados= new ArrayList<Partido>();
+        this.puntajeEtapaGrupos=0;
+    }
+
+    public Equipo(String nombre, ArrayList<Partido> partidosJugados) {
+        this.nombre = nombre;
+        this.partidosJugados = partidosJugados;
+        this.puntajeEtapaGrupos=0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Partido> getPartidosJugados() {
+        return partidosJugados;
+    }
+
+    public void setPartidosJugados(ArrayList<Partido> partidosJugados) {
+        this.partidosJugados = partidosJugados;
+    }
+
+    public int getPuntajeEtapaGrupos() {
+        return puntajeEtapaGrupos;
+    }
+
+    public void setPuntajeEtapaGrupos(int puntajeEtapaGrupos) {
+        this.puntajeEtapaGrupos = puntajeEtapaGrupos;
+    }
+    public void addPartidosJugados(Partido partido) {
+        this.partidosJugados.add(partido);
+    }
+
+    public void partidoGanado(){
+        this.puntajeEtapaGrupos += 3;
+    }
+
+    public void partidoEmpatado(){
+        this.puntajeEtapaGrupos += 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" +
+                "nombre='" + nombre + '\'' +
+                ", partidosJugados=" + partidosJugados +
+                ", puntajeEtapaGrupos=" + puntajeEtapaGrupos +
+                '}';
+    }
 }
---
+++ Mundial/src/Partido.java
@@ -0,0 +1,2 @@
+public class Partido {
+}
