a715e512b639c56f860dcd1b41b887eff3a3df5d
Keitty <Keila.degregorio@davinci.edu.ar>
2022-12-05 01:42:18 -0300

    feat: add setter and getter in Contacto

--- Garaje/src/vehiculos/Contacto.java
+++ Garaje/src/vehiculos/Contacto.java
@@ -1,4 +1,46 @@
 package vehiculos;
 
 public class Contacto {
+    private String nombre;
+    private String apellido;
+    private int numeroCelular;
+
+    public Contacto(String nombre, String apellido, int numeroCelular) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroCelular = numeroCelular;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(int numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", numeroCelular=" + numeroCelular +
+                '}';
+    }
 }
