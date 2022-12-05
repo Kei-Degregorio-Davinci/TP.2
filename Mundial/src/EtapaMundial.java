176b583c964c93c2089db952eaf5213aa09feae2
Keitty <Keila.degregorio@davinci.edu.ar>
2022-12-05 01:25:52 -0300

    feat: add setter and getter in Class EtapaMundial

--- Mundial/src/EtapaMundial.java
+++ Mundial/src/EtapaMundial.java
@@ -1,2 +1,60 @@
+import java.util.ArrayList;
+
 public abstract class EtapaMundial {
+    private String descripcionEtapa;
+    private ArrayList<Partido> partidos;
+    private ArrayList<Equipo> equipoQueAvanza;
+
+    public EtapaMundial() {
+        super();
+        this.partidos= new ArrayList<Partido>();
+        this.equipoQueAvanza= new ArrayList<Equipo>();
+    }
+
+    public EtapaMundial(String descripcionEtapa, ArrayList<Partido> partidos, ArrayList<Equipo> equipoQueAvanza) {
+        this.descripcionEtapa = descripcionEtapa;
+        this.partidos = partidos;
+        this.equipoQueAvanza = equipoQueAvanza;
+    }
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+
+    public void setDescripcionEtapa(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
+    }
+
+    protected ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+
+    public ArrayList<Equipo> getEquipoQueAvanza() {
+        return equipoQueAvanza;
+    }
+
+    public void setEquipoQueAvanza(ArrayList<Equipo> equipoQueAvanza) {
+        this.equipoQueAvanza = equipoQueAvanza;
+    }
+
+    public void addPartido(Partido partido){
+        this.partidos.add(partido);
+    }
+
+    public void addEquipoQueAvanza(Equipo equipo){
+        this.equipoQueAvanza.add(equipo);
+    }
+
+    @Override
+    public String toString() {
+        return "EtapaMundial{" +
+                "descripcionEtapa='" + descripcionEtapa + '\'' +
+                ", partidos=" + partidos +
+                ", equipoQueAvanza=" + equipoQueAvanza +
+                '}';
+    }
 }
