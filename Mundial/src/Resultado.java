e1c10a4dc1ec62791e40e791e385e01ff600296f
Keitty <Keila.degregorio@davinci.edu.ar>
2022-12-05 01:28:15 -0300

    feat: add setter and getter in class Resultado

--- Mundial/src/Resultado.java
+++ Mundial/src/Resultado.java
@@ -1,4 +1,58 @@
 public class Resultado {
+    private int golesLocal;
+    private int golesVisitante;
 
+    public Resultado(int golesLocal, int golesVisitante) {
+        this.golesLocal = golesLocal;
+        this.golesVisitante = golesVisitante;
+    }
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+
+    public boolean ganoLocal() {
+        if (this.golesLocal>this.golesVisitante){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean ganoVisitante(){
+        if (this.golesVisitante>this.golesLocal){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean empate(){
+        if (this.golesLocal == this.golesVisitante){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Resultado{" +
+                "golesLocal=" + golesLocal +
+                ", golesVisitante=" + golesVisitante +
+                '}';
+    }
 }
 
