51be2247352cf9ef4d3f0580ff4f3ed767555895
Keitty <Keila.degregorio@davinci.edu.ar>
2022-12-05 01:27:08 -0300

    feat: add get in Class Grupo and in Class Llave

--- Mundial/src/Grupo.java
+++ Mundial/src/Grupo.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Grupo extends EtapaMundial{
+    private ArrayList<Equipo> equipoQueAvanza;
+
+    public Grupo(){
+        this.equipoQueAvanza= new ArrayList<Equipo>();
+    }
+
+    public Grupo(ArrayList<Equipo> equipoQueAvanza){
+        this.equipoQueAvanza= equipoQueAvanza;
+    }
+
+    @Override
+    public ArrayList<Equipo> getEquipoQueAvanza() {
+        return super.getEquipoQueAvanza();
+    }
 }
--- Mundial/src/Llave.java
+++ Mundial/src/Llave.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Llave extends EtapaMundial{
+    private String nombre;
+    private ArrayList<Equipo> equipoQueAvanza;
+
+    public Llave(String nombre){
+        super();
+    }
+
+    @Override
+    public ArrayList<Equipo> getEquipoQueAvanza() {
+        return super.getEquipoQueAvanza();
+    }
 }
